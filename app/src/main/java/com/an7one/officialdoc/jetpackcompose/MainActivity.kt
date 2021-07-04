30cef4d87e0eebcec37c00884fb91cc551870489
Leon <yanglyu.leon.7@gmail.com>
2021-07-04 15:43:35 -0700

    tutorial: layouts

--- app/src/main/java/com/an7one/officialdoc/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/an7one/officialdoc/jetpackcompose/MainActivity.kt
@@ -3,26 +3,48 @@ package com.an7one.officialdoc.jetpackcompose
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting("Android")
+            NewsStory()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun NewsStory() {
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.header),
+            contentDescription = null,
+            modifier = Modifier
+                .height(180.dp)
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(text = "A day in Shark Fin Cove")
+        Text(text = "Davenport, California")
+        Text(text = "December 2018")
+    }
 }
 
 @Preview
 @Composable
-fun PreviewGreeting(){
-    Greeting("Android")
+fun PreviewGreeting() {
+    NewsStory()
 }
