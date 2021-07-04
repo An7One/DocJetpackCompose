dcc2eeb10166886938c3b8a6a42fa2739eae5ed2
Leon <yanglyu.leon.7@gmail.com>
2021-07-04 15:49:25 -0700

    tutorial: material design

--- app/src/main/java/com/an7one/officialdoc/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/an7one/officialdoc/jetpackcompose/MainActivity.kt
@@ -5,9 +5,12 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,14 +35,24 @@ fun NewsStory() {
             contentDescription = null,
             modifier = Modifier
                 .height(180.dp)
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(4.dp)),
             contentScale = ContentScale.Crop
         )
         Spacer(modifier = Modifier.height(16.dp))
 
-        Text(text = "A day in Shark Fin Cove")
-        Text(text = "Davenport, California")
-        Text(text = "December 2018")
+        Text(
+            text = "A day in Shark Fin Cove",
+            style = typography.h6
+        )
+        Text(
+            text = "Davenport, California",
+            style = typography.body2
+        )
+        Text(
+            text = "December 2018",
+            style = typography.body2
+        )
     }
 }
 
